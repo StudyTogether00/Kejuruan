ca3249994a00522caed00ccf3c5f71cd99013bdc
noveliaramadhani <138117181+noveliaramadhani@users.noreply.github.com>
2024-08-29 23:11:45 +0700

    up API baru

--- app/Http/Controllers/Jurusancontroller.php
+++ app/Http/Controllers/Jurusancontroller.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Jurusan;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
 
 class Jurusancontroller extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $data = Jurusan::all();
+        return response ()->json(["data" =>$data]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('whome');
+});
+Route::post('/test', function () {
+    return 'test method home';
 });
