cb5d485b179cd4ba77df5f4e83eb0d64b5832496
noveliaramadhani <138117181+noveliaramadhani@users.noreply.github.com>
2024-09-03 23:06:47 +0700

    api2

--- app/Http/Controllers/Jurusancontroller.php
+++ app/Http/Controllers/Jurusancontroller.php
@@ -9,6 +9,34 @@ use Illuminate\Support\Facades\Route;
 
 class Jurusancontroller extends Controller
 {
+    public function lists(Request $request)
+    {
+        try {
+            $data = Jurusan::select("kd_jurusan","nama_jurusan")->get();
+            $respon =[
+            "status" => true,
+            "message" => "List Data Jurusan",
+            "data" => $data,
+            ];
+            $code = 200;
+        }catch (\Throwable $th) {
+           $respon = [
+            "status" => false,
+            "message" => $th->getMessage(),
+            "data" => [],
+            ];
+            $code = 400;
+       }
+        return response()->json($respon,$code);
+    }
+    public function Add(Request $request)
+    {
+        
+    }
+    public function Delete(Request $request)
+    {
+        
+    }
     public function index(Request $request)
     {
         $data = Jurusan::all();
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post("/test", [Jurusancontroller::class,"index"]);
+
+Route::post("/List", [Jurusancontroller::class,"Lists"]);
